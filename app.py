00f163cc367c746b2e5a65b251b7c0dd9c1a8f91
Kyle Mistele <kyle@mistele.com>
2023-04-02 17:29:21 -0500

    use API blueprint in-app

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
+from blueprints.api_blueprint import api_blueprint
 
 app = Flask(__name__)
+app.register_blueprint(api_blueprint)
 
 @app.route('/')
 def index():
     return '<h1>First Flask App</h1>'
 
-    
 
 if __name__ == '__main__':
     app.run(debug = True, port=3000)
