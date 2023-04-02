0a3bd0a5a4251e9d323047061e2c4cca1619bdf2
Kyle Mistele <kyle@mistele.com>
2023-04-02 17:44:09 -0500

    added an API route to get a random word from a list

--- blueprints/api_blueprint.py
+++ blueprints/api_blueprint.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, jsonify
 import time
+import random
+
 
 # create a flask blueprint for me
 api = Blueprint('api', __name__)
@@ -10,5 +12,17 @@ api_blueprint = Blueprint('api', __name__, url_prefix='/api')
 def current_time():
 	return jsonify({
 		'time': time.time()
+	})
+
+
+@api_blueprint.route('/some-word')
+def some_word():
+
+	words = ['apple', 'cat', 'dog', 'banana', 'hello', 'world']
+	# get a random word from the list
+	random_word = random.choice(words)
+
+	return jsonify({
+		'word': random_word
 	})
 	
